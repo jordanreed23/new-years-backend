22c360368f54553d72d735b5f3f47d8ab3ed4018
Jordan Reed <jordanr23@gmail.com>
2018-01-03 10:16:05 -0700

    patch routes

--- routes/users.js
+++ routes/users.js
@@ -21,21 +21,21 @@ router.post('/:id/new', (req,res,next) => {
   })
 })
 
-router.post('/:id/increment', (req,res,next) => {
+router.patch('/:id/increment', (req,res,next) => {
   db.updateStreak(req.body)
   .then( data => {
     res.redirect('/users/' + req.params.id)
   })
 })
 
-router.post('/:id/startOver', (req,res,next) => {
+router.patch('/:id/startOver', (req,res,next) => {
   db.startOver(req.body)
   .then( data => {
     res.redirect('/users/' + req.params.id)
   })
 })
 
-router.post('/:id/delete', (req,res,next) => {
+router.patch('/:id/delete', (req,res,next) => {
   db.deleteResolution(req.body)
   .then( data => {
     res.redirect('/users/' + req.params.id)
